8ae9dcdfa667e4e8c9b7363df2cc2526b6226e19
362700239 <jennarbloor@gmail.com>
2023-04-12 11:00:23 -0400

    fix inflate problem

--- Monogame Lesson 3 – Animation/Game1.cs
+++ Monogame Lesson 3 – Animation/Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
@@ -30,7 +31,9 @@ namespace Monogame_Lesson_3___Animation
         int randomX3;
         int randomY3;
         int randomX4;
-        int randomY4;       
+        int randomY4;
+        int inflateX;
+        int inflateY;      
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -56,7 +59,7 @@ namespace Monogame_Lesson_3___Animation
             randomX3 = (generator.Next(0, 700));
             randomY3 = (generator.Next(0, 500));
             randomX4 = (generator.Next(0, 700));
-            randomY4 = (generator.Next(0, 500));
+            randomY4 = (generator.Next(0, 500));           
             greyTribbleRect = new Rectangle(randomX, randomY, 100, 100);
             tribbleGreySpeed = new Vector2(2, 2);
             brownTribbleRect = new Rectangle(randomX2, randomY2, 100, 100);
@@ -66,6 +69,8 @@ namespace Monogame_Lesson_3___Animation
             orangeTribbleRect = new Rectangle(randomX4, randomY4, 100, 100);
             tribbleOrangeSpeed = new Vector2(2, 4);
             spaceshipRect = new Rectangle(0, 0, 803, 603);
+            inflateX = 1;
+            inflateY = 1;
 
             base.Initialize();
         }
@@ -85,27 +90,33 @@ namespace Monogame_Lesson_3___Animation
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+                    
             greyTribbleRect.X += (int)tribbleGreySpeed.X;
             greyTribbleRect.Y += (int)tribbleGreySpeed.Y;
-            if (greyTribbleRect.Right > _graphics.PreferredBackBufferWidth || greyTribbleRect.Left < 0)
-                tribbleGreySpeed.X *= -1;             
-            if (greyTribbleRect.Bottom > _graphics.PreferredBackBufferHeight || greyTribbleRect.Top < 0)
-                tribbleGreySpeed.Y *= -1;              
+            if (greyTribbleRect.Right > _graphics.PreferredBackBufferWidth || greyTribbleRect.Left < 0)               
+                tribbleGreySpeed.X *= -1;               
+            if (greyTribbleRect.Bottom > _graphics.PreferredBackBufferHeight || greyTribbleRect.Top < 0)              
+                tribbleGreySpeed.Y *= -1;    
+            
             brownTribbleRect.X += (int)tribbleBrownSpeed.X;
             brownTribbleRect.Y += (int)tribbleBrownSpeed.Y;
-            if (brownTribbleRect.Right > _graphics.PreferredBackBufferWidth || brownTribbleRect.Left < 0)
-                tribbleBrownSpeed.X *= -1;
+            if (brownTribbleRect.Right > (_graphics.PreferredBackBufferWidth + 100) || brownTribbleRect.Left < -100)
+                brownTribbleRect.X = -100;     
+            
             creamTribbleRect.X += (int)tribbleCreamSpeed.X;
             creamTribbleRect.Y += (int)tribbleCreamSpeed.Y;
             if (creamTribbleRect.Bottom > _graphics.PreferredBackBufferHeight || creamTribbleRect.Top < 0)
                 tribbleCreamSpeed.Y *= -1;
+                //inflateX *= -1;
+                //inflateY *= -1;           
+                creamTribbleRect.Inflate(inflateX * -1, inflateY * -1);
+
             orangeTribbleRect.X += (int)tribbleOrangeSpeed.X;
             orangeTribbleRect.Y += (int)tribbleOrangeSpeed.Y;
             if (orangeTribbleRect.Right > _graphics.PreferredBackBufferWidth || orangeTribbleRect.Left < 0)
                 tribbleOrangeSpeed.X *= -1;
             if (orangeTribbleRect.Bottom > _graphics.PreferredBackBufferHeight || orangeTribbleRect.Top < 0)
-                tribbleOrangeSpeed.Y *= -1;
+                tribbleOrangeSpeed.Y *= -1;           
 
             base.Update(gameTime);
         }
